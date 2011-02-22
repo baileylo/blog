ec3ced2c0f24959fe90b7b1462a1f1fba0be3180
Logan Bailey <logan@logansbailey.com>
2011-02-21 23:30:50 -0800

    Add users to posts that were made prior to user migration

---
+++ db/migrate/20110222070136_insert_user_in_all_posts.rb
@@ -0,0 +1,13 @@
+class InsertUserInAllPosts < ActiveRecord::Migration
+  def self.up
+    user = User.find(:first)
+    Post.where(:user_id => nil).each do |post|
+      post.user = user;
+      post.save
+    end
+    
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110221020819) do
+ActiveRecord::Schema.define(:version => 20110222070136) do
 
   create_table "posts", :force => true do |t|
     t.text     "body"
