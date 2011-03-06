ca041f693d00410d401bf75ff801f0b341f8b78e
Logan Bailey <logan@logansbailey.com>
2011-03-05 23:38:17 -0800

    Added validation for username, devise, and set up username to be publicy editable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ActiveRecord::Base
+  validates_uniqueness_of :username
+  validates_length_of :username, :in => 3..15
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+  devise :database_authenticatable, :registerable, :validatable,
+         :recoverable, :rememberable, :trackable, :token_authenticatable
 
   has_many :posts
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
 end
